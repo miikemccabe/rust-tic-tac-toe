2a9255fd8f5aaf62ba26d6415e3f9be424f171b8
Mike McCabe <mike.mccabe@missguided.com>
2021-08-22 11:17:21 +0100

    Slightly better error handling (arguably)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,68 @@ mod game;
 
 use game::Game;
 
-fn parse_choice(cell: &str) -> Result<(usize, usize), ()> {
+enum Command {
+    Clear,
+    Quit
+}
+
+enum Choice {
+    Cell((usize, usize)),
+    Command(Command)
+}
+
+enum ChoiceError {
+    InvalidCoords(String),
+    CommandNotRecognised,
+}
+
+struct CoordsError(String);
+
+fn get_choice() -> Result<Choice, ChoiceError> {
+    
+    let mut cell = String::new();
+
+    io::stdin()
+        .read_line(&mut cell)
+        .expect("Failed to read line");
+
+    let choice = cell.trim();
+
+    let command = parse_command(choice);
+    let coords = parse_coords(choice);
+
+    if command.is_err() && coords.is_err() {
+        return Err(ChoiceError::CommandNotRecognised);
+    }
+
+    match command {
+        Ok(command) => Ok(Choice::Command(command)),
+        Err(()) => match coords {
+            Ok(coords) => Ok(Choice::Cell(coords)),
+            Err(CoordsError(message)) => Err(ChoiceError::InvalidCoords(message))
+        }
+    }
+}
+
+fn parse_command(command: &str) -> Result<Command, ()> {
+    match command {
+        "quit" => Ok(Command::Quit),
+        "q" => Ok(Command::Quit),
+        "clear" => Ok(Command::Clear),
+        "c" => Ok(Command::Clear),
+        _ => Err(())
+    }
+}
+
+fn parse_coords(cell: &str) -> Result<(usize, usize), CoordsError> {
+
+    if cell.len() > 2 {
+       return Err(CoordsError(String::from("Too many coordinates")))
+    }
+
+    if cell.len() < 2 {
+       return Err(CoordsError(String::from("Too few coordinates")))
+    }
 
     // The user should enter a two digit coordinate for the cell like A2 or 3B. Splitting on "" produces an empty
     // string on either end of the array so we filter these about before collecting them as a Vector of &str.
@@ -14,53 +75,55 @@ fn parse_choice(cell: &str) -> Result<(usize, usize), ()> {
     // Sort the array so that the user can enter the row or column in either order
     cell.sort_unstable();
 
-    let column = cell[0].parse::<usize>().unwrap_or(0) - 1;
+    let column = cell[0].parse::<usize>();
+    
+    if column.is_err() {
+        return Err(CoordsError(String::from("Can't parse column")));
+    }
 
-    let row =  match cell[1].to_lowercase().as_ref() {
-        "a" => 0,
-        "b" => 1,
-        "c" => 2,
-        _ => 0
+    let row: Option<usize> =  match cell[1].to_lowercase().as_ref() {
+        "a" => Some(0),
+        "b" => Some(1),
+        "c" => Some(2),
+        _ => None
     };
+    
+    if row.is_none() {
+        return Err(CoordsError(String::from("Can't parse row")));
+    }
 
-    Ok((row, column))
-
+    Ok((row.unwrap(), column.unwrap() - 1))
 }
 
 fn main() {
 
     let mut game = Game::new();
 
-    game.display();
-
     loop {
+
+        game.display();
 
         println!("Player {}, choose your cell", game.player);
 
-        let mut cell = String::new();
+        let choice = get_choice();
 
-        io::stdin()
-            .read_line(&mut cell)
-            .expect("Failed to read line");
-
-        let choice = cell.trim();
-
-        if choice == "c" {
-            game.reset_board();
-            game.display();
-            continue;
-        } else if choice == "q" {
-            println!("Thank you for playing!");
-            break;
+        match choice {
+            Ok(Choice::Command(command)) => {
+                match command {
+                    Command::Quit => break,
+                    Command::Clear => {
+                        game.reset_board();
+                        game.display();
+                        continue;
+                    }
+                }
+            },
+            Ok(Choice::Cell((row, col))) => {
+                game.play_turn(row, col);
+            },
+            Err(ChoiceError::CommandNotRecognised) => println!("Error: Command not recognised"),
+            Err(ChoiceError::InvalidCoords(message)) => println!("Error: {}", message),
         }
-
-        let (row, col) = parse_choice(choice).unwrap();
-
-        game.play_turn(row, col);
-
-        println!("{:?}", game.board);
-
-        game.display();
 
     }
 }
